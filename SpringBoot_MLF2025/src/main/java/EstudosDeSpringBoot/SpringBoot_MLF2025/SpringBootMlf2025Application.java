07b0c421e157cf25620cec49f2c61c11200c3abd
PassosLucasB <passosl@outlook.com>
2026-06-07 21:50:47 -0300

    Mudança de lugar do arquivo corrigindo erro

---
+++ SpringBoot_MLF2025/src/main/java/EstudosDeSpringBoot/SpringBoot_MLF2025/SpringBootMlf2025Application.java
@@ -0,0 +1,13 @@
+package EstudosDeSpringBoot.SpringBoot_MLF2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMlf2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMlf2025Application.class, args);
+	}
+
+}
---
+++ SpringBoot_MLF2025/src/test/java/EstudosDeSpringBoot/SpringBoot_MLF2025/SpringBootMlf2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package EstudosDeSpringBoot.SpringBoot_MLF2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMlf2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
